9e01ac3637eb5a57ae44c52073ab1588e088c7e3
Marcello Zamora <zamoramaj@gmail.com>
2019-08-01 21:32:39 -0400

    initial commit

---
+++ Scenes/Scripts/Player.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+export var speed = 16
+var velocity = Vector2(0,0)
+
+func get_input():
+	velocity = Vector2(0,0)
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	velocity = velocity.normalized() * speed
+
+func _physics_process(delta):
+	get_input()
+	velocity = move_and_slide(velocity)
